e33e3a4eb06270943d84fc0667cb2bc46aedfa3e
Kirill <tor505274@gmail.com>
2021-06-03 20:53:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleCPP3/ConsoleCPP3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    int arr[20];
+    int otherArr[20];
+    int* pArr =arr;
+    int* pOther=otherArr;
+    char c;
+    int length = 0;
+    while ((c = fgetc(stdin)) != '\n') {
+        *(pArr+length)=c-'0';
+        length++;
+    }
+
+    for (int i = 0; i < length/2; i++) {
+        *(pOther + 2*i) = *(pArr + 2*i + 1);
+        *(pOther + 2*i+1) = *(pArr + 2*i);
+    }
+    if(length%2==1){
+        *(pOther + length-1) = *(pArr + length-1);
+    }
+    for (int i = 0; i < length; i++) {
+        cout <<*(pOther+i) ;
+    }
+    return 0;
+}
